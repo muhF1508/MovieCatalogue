6cd0d15e3f2a1396d397061b05032d94f00990f1
aminfaruq <43925789+aminfaruq@users.noreply.github.com>
2019-02-21 18:26:40 +0700

    Update ApiInterface.java

--- app/src/main/java/id/co/maminfaruq/moviecatalogueapi/network/ApiInterface.java
+++ app/src/main/java/id/co/maminfaruq/moviecatalogueapi/network/ApiInterface.java
@@ -7,9 +7,9 @@ import retrofit2.http.GET;
 
 public interface ApiInterface {
 
-    @GET("/3/movie/now_playing?api_key={API KEY}&language=en-US")
+    @GET("/3/movie/now_playing?api_key=5ba01fa664bad862f5c8f6cbbec5f291&language=en-US")
     Call<ResponseNow>getNowMovie();
 
-    @GET("/3/movie/upcoming?api_key={API KEY}&language=en-US")
+    @GET("/3/movie/upcoming?api_key=5ba01fa664bad862f5c8f6cbbec5f291&language=en-US")
     Call<ResponseUp>getUpMovie();
 }
